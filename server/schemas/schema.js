ea8494acbfbcbdae4ba4f1badc17b8acc5e753d4
Awadhesh Kumar <you@example.com>
2021-10-03 02:06:26 +0530

    schema updated

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,10 +1,15 @@
 const graphQL = require("graphql")
-const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLString } = graphQL
+const { GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLID, GraphQLString } = graphQL
 
 const Books = [
-    { id: "1", name: "apple", genre: "tech" },
-    { id: "2", name: "samsung", genre: "tech" },
-    { id: "3", name: "nokia", genre: "tech" }
+    { id: "1", name: "apple", genre: "tech", authorId: "1" },
+    { id: "2", name: "samsung", genre: "tech", authorId: "2" },
+    { id: "3", name: "nokia", genre: "tech", authorId: "1" }
+]
+
+const Authors = [
+    { id: "1", name: "awadhesh", age: "27" },
+    { id: "2", name: "kumar", age: "26" }
 ]
 
 const BookType = new GraphQLObjectType({
@@ -12,13 +17,42 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return Authors.find(index => index.id === parent.authorId)
+            }
+        }
     })
 })
 
-const BookTypeQuery = new GraphQLObjectType({
-    name: "BookQuery",
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLString },
+        book: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return Books.filter(index => index.authorId === parent.id)
+            }
+        }
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: "RootQuery",
     fields: {
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Authors.find(index => index.id === args.id)
+            }
+        },
         book: {
             type: BookType,
             args: { id: { type: GraphQLID } },
@@ -30,5 +64,5 @@ const BookTypeQuery = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({
-    query: BookTypeQuery
+    query: RootQueryType,
 })
